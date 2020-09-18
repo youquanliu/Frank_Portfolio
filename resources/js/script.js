611a5ef2128dea80957fbb0c8181fd5a9c411cb9
LIU FRANK <youquan.liu@hotmail.com>
2020-09-18 17:20:44 -0400

    add projects filter feature

--- resources/js/script.js
+++ resources/js/script.js
@@ -10,6 +10,23 @@ $(document).ready(function () {
         alert('eeeeeeeeeee');
     })
 
+    // PROJECTS SECTION FILTER
+    $('.itemBox').on('click', function () {
+        $(this).toggleClass('show-description');
+    });
+    
+    $('.list').click(function () {
+        const value = $(this).attr('data-filter');
+        if (value == 'all') {
+            $('.itemBox').show('1000');
+        } else {
+            $('.itemBox').not('.' + value).hide('1000');
+            $('.itemBox').filter('.' + value).show('1000');
+        }
+    })
+    $('.list').click(function () {
+        $(this).addClass('active').siblings().removeClass('active');
+    })
 
     // reorganizeIsotope();
     // //preloader 
