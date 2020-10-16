481b8f2522cd4f4f7bcd3022cf3bcd69f21a3d44
LIU FRANK <youquan.liu@hotmail.com>
2020-10-15 22:57:40 -0400

    done on the way section animation

--- resources/js/parallax.js
+++ resources/js/parallax.js
@@ -4,14 +4,16 @@ let mountain = document.getElementById('mountain');
 let road = document.getElementById('road');
 let text = document.getElementById('parallax-text');
 
-window.addEventListener('scroll', function() {
-    var value = window.scrollY;
-    console.log(value);
+const sec = document.querySelector('.section-onTheWay');
+
+
+window.addEventListener('scroll', function () {
+
+    var value = window.scrollY - sec.offsetTop;
 
     bg.style.top = value * 0.5 + 'px';
     moon.style.left = -value * 0.5 + 'px';
     mountain.style.top = -value * 0.15 + 'px';
     road.style.top = value * 0.15 + 'px';
-    text.style.top = value * 1 + 'px';
-
+    text.style.top = value * 0.5 + 'px';
 })
