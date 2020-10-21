0ec94ed27385e9abdc8384f25ea309c1ef497e4f
LIU FRANK <youquan.liu@hotmail.com>
2020-10-21 17:57:09 -0400

    1.add my skills section imgages 2.add my skills section animation

--- resources/js/parallax.js
+++ resources/js/parallax.js
@@ -1,3 +1,5 @@
+
+//on the way section parallax
 let bg = document.getElementById('bg');
 let moon = document.getElementById('moon');
 let mountain = document.getElementById('mountain');
@@ -16,3 +18,16 @@ window.addEventListener('scroll', function () {
     road.style.top = value * 0.15 + 'px';
     text.style.top = value * 0.5 + 'px';
 })
+
+//techs section parallax
+document.addEventListener("mousemove", parallax);
+function parallax(e) {
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed')
+
+        const x = (window.innerWidth - e.pageX * speed) / 100
+        const y = (window.innerHeight - e.pageY * speed) / 100
+
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`
+    });
+}
